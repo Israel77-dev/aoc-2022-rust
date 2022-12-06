fde1e3c351b51b644157ab837520a88dabe538bb
Israel Batista <dev.israel.batista@gmail.com>
2022-12-06 10:56:28 -0300

    Template for compiling daily solutions

---
+++ template/src/lib.rs
@@ -0,0 +1,22 @@
+pub fn process_part_1(input: &str) -> usize {
+    todo!()
+}
+
+pub fn process_part_2(input: &str) -> usize {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn part_1() {
+        todo!()
+    }
+
+    #[test]
+    fn part_2() {
+        todo!()
+    }
+}
