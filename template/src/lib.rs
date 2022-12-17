b763fa10e8f5f8c4bf57342a93e79447312f7917
Israel Batista <dev.israel.batista@gmail.com>
2022-12-16 21:49:36 -0300

    Test input is now in a separate file

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -8,15 +8,25 @@ pub fn process_part_2(input: &str) -> usize {
 
 #[cfg(test)]
 mod tests {
+    use std::fs;
+
     use super::*;
 
     #[test]
     fn part_1() {
+        let input = fs::read_to_string("./test.txt").unwrap();
+        //assert_eq(
+        process_part_1(&input);
+        //,  )
         todo!()
     }
 
     #[test]
     fn part_2() {
+        let input = fs::read_to_string("./test.txt").unwrap();
+        //assert_eq(
+        process_part_2(&input);
+        //,  )
         todo!()
     }
 }
